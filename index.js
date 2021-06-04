d14e52b9dfab570b354cc0b94c51da6d4e398c77
ivnicolas <ikennanicolas@gmail.com>
2021-06-04 13:06:33 -0400

    commit tree 15ce46f11226fd292db09a0098c5ee15347fa334

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Your code here
+
+function saturdayFun(task = "roller-skate"){
+    return `This Saturday, I want to ${task}!`
+}
+
+function mondayWork(work = "go to the office"){
+    return  `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(flair='*'){
+    return function(word){
+        return `You are ${flair}${word}${flair}!`
+    }
+}
+
+
+let Calculator = {
+    add: function(a,b){return a + b
+    },
+    subtract:function(a,b){return a - b
+    },
+    multiply:function(a,b){return a * b
+    },
+    divide:function(a,b){return a / b
+    },
+    
+}
+
+function actionApplyer(start, ray){
+    let a = start
+
+    for (let i = 0; i < ray.length; i++ ){
+      a = ray[i](a)
+    }
+  
+    return a
+
+}
+
+// function(){
+//     let Calculator = {}
+// }
+// let calculator -
+// let calculator = {
+//     return function add(a,b){
+//         return `${a} + ${b}`
+//     },
+//     return function subtract(a,b){
+//         return `${a}  ${b}`
+//     },
+//     return function multiply(a,b){
+//         return `${a} * ${b}`
+//     },
+//     return function device(a,b){
+//         return `${a} / ${b}`
+//     }
+// }
